d75bdca5a6014fead332830586d2dcb61d460254
U-MSI\Puch <josera.baez@gmail.com>
2017-06-17 23:45:08 -0500

    change to c, change HOST conditional def

--- pk_hama_dbg_trace_user/hama_dbg_trace.h
+++ pk_hama_dbg_trace_user/hama_dbg_trace.h
@@ -17,9 +17,8 @@
 /*=====================================================================================* 
  * Standard Includes
  *=====================================================================================*/
-#if HOST
-#include <cstdio>
-#include <iostream>
+#ifdef HOST
+#include <stdio.h>
 #include <unistd.h>
 /*=====================================================================================* 
  * Exported X-Macros
@@ -29,21 +28,25 @@
  * Exported Define Macros
  *=====================================================================================*/
 #define TR_PRINT_FILE printf("TR_INFO %s %d -  ", __FILE__, __LINE__)
-#define TR_PRINT_LINE std::cout << std::endl; usleep(250000);
+#define TR_PRINT_LINE printf("\n"); usleep(250000);
+
 
 #else
 #include "arduino_fwk_uart.h"
+#endif
+
+#ifdef __cplusplus
+#include <iostream>
+extern "C" {
 #endif
 /*=====================================================================================* 
  * Exported Type Declarations
  *=====================================================================================*/
 
-namespace dbg
-{
 /*=====================================================================================* 
  * Exported Object Declarations
  *=====================================================================================*/
-#if HOST
+#ifdef HOST
 
 #else
 extern const Arduino_UART_T UART_Init;
@@ -54,11 +57,10 @@ extern const char CR[] PROGMEM;
  *=====================================================================================*/
    extern void Init(void);
    extern void Shut(void);
-}
 /*=====================================================================================* 
  * Exported Function Like Macros
  *=====================================================================================*/
-#if HOST
+#ifdef HOST
 
 #define TR_INFO(msg)                       TR_PRINT_FILE; printf(msg);                    TR_PRINT_LINE
 #define TR_INFO_1(msg, a1)                 TR_PRINT_FILE; printf(msg, a1);                TR_PRINT_LINE
@@ -79,6 +81,10 @@ extern const char CR[] PROGMEM;
 #define TR_INFO_4(msg, a1, a2, a3, a4)     static const char TR_INFO_INS(tr_file, __LINE__)[] PROGMEM = __FILE__ " \0"; arduino::Print_UART_P( dbg::UART_Init.channel, TR_INFO_INS(tr_file, __LINE__) ); arduino::Print_UART( dbg::UART_Init.channel, __LINE__);  static const char TR_INFO_INS(tr_info, __LINE__)[] PROGMEM = " - "msg "\0"; arduino::Print_UART_P( dbg::UART_Init.channel, TR_INFO_INS(tr_info, __LINE__) ); arduino::Print_UART(dbg::UART_Init.channel, a1); arduino::Print_UART(dbg::UART_Init.channel,a2); arduino::Print_UART(dbg::UART_Init.channel, a3); arduino::Print_UART(dbg::UART_Init.channel, a4); arduino::Print_UART_P(dbg::UART_Init.channel, dbg::CR)
 #define TR_INFO_5(msg, a1, a2, a3, a4, a5) static const char TR_INFO_INS(tr_file, __LINE__)[] PROGMEM = __FILE__ " \0"; arduino::Print_UART_P( dbg::UART_Init.channel, TR_INFO_INS(tr_file, __LINE__) ); arduino::Print_UART( dbg::UART_Init.channel, __LINE__);  static const char TR_INFO_INS(tr_info, __LINE__)[] PROGMEM = " - "msg "\0"; arduino::Print_UART_P( dbg::UART_Init.channel, TR_INFO_INS(tr_info, __LINE__) ); arduino::Print_UART(dbg::UART_Init.channel, a1); arduino::Print_UART(dbg::UART_Init.channel,a2); arduino::Print_UART(dbg::UART_Init.channel, a3); arduino::Print_UART(dbg::UART_Init.channel, a4); arduino::Print_UART(dbg::UART_Init.channel, a5); arduino::Print_UART_P(dbg::UART_Init.channel, dbg::CR)
 
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 /*=====================================================================================* 
  * hama_dbg_trace.

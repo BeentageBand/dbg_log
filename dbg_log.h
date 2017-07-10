0f17e47ee7e92c1a608a2fc9b54d4409b6776a31
Puch <josera.baez@gmail.com>
2017-07-10 18:51:09 -0500

    dbg_log - _args functional in dbg log

--- dbg_log.h
+++ dbg_log.h
@@ -49,7 +49,7 @@ extern void Dbg_Log_Print(Dbg_Feat_Id_T const fid, uint8_t const instance, char 
 #define Dbg_Info(_args...) \
 		Dbg_Log_Print(Dbg_FID, DBG_INFO, __LINE__, _args)
 
-#define Dbg_Warn(args...) \
+#define Dbg_Warn(_args...) \
 		Dbg_Log_Print(Dbg_FID, DBG_WARN, __LINE__, _args)
 
 #ifdef __cplusplus

35f60deb67b9599bcda248b024d23c45ff9ff7cd
uids6144 <roberto.alanis@continental-corporation.com>
2018-01-28 11:17:34 -0600

    add ifdef plus plus for *.cpp usage

--- dbg_log_ext.h
+++ dbg_log_ext.h
@@ -4,9 +4,16 @@
 #include <stdarg.h>
 #include "std_reuse.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern struct Dbg_Log_Stdout
 {
 	int (*vsprintf)(char const *, uint8_t const, va_list);
 } const Dbg_Stdout;
 
+#ifdef __cplusplus
+}
+#endif
 #endif /*DBG_LOG_EXT_H_*/

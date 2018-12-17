dadf0288a73ec64e8239655cc42129ac0fb710c6
Roberto Alanis <roalanis@amazon.com>
2018-12-16 20:28:51 -0800

    support macos

--- dbg_log_make.mk
+++ dbg_log_make.mk
@@ -2,10 +2,13 @@ define $(_flavor_)_$(_feat_)_MAKE
 
 $(_flavor_)_$(_feat_)_inc=dbg_log.h dbg_log_ext.h dbg_log_types.h 
 
-$(_flavor_)_$(_feat_)_lib_objs=dbg_log $($(_flavor_)_build:%=dbg_log_%)
+$(_flavor_)_$(_feat_)_lib_objs=dbg_log
 
 $(_flavor_)_$(_feat_)_lib=dbg_log
 
 endef
+
+macos_$(_feat_)_lib_objs+=dbg_log_linux.c
+linux_$(_feat_)_lib_objs+=dbg_log_linux.c
 
 include $(PROJ_MAK_DIR)/epilog.mk

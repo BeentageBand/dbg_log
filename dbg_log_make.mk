13d3194024e8754d1861d3c39cdaec1e59c525cd
BeentageBand <josera.baez@gmail.com>
2019-02-17 18:18:00 -0800

    rm linux imp for now

--- dbg_log_make.mk
+++ dbg_log_make.mk
@@ -2,13 +2,10 @@ define $(_flavor_)_$(_feat_)_MAKE
 
 $(_flavor_)_$(_feat_)_inc=dbg_log.h dbg_log_ext.h dbg_log_types.h 
 
-$(_flavor_)_$(_feat_)_lib_objs=dbg_log
+$(_flavor_)_$(_feat_)_lib_objs=dbg_log dbg_log_linux
 
 $(_flavor_)_$(_feat_)_lib=dbg_log
 
 endef
-
-macos_$(_feat_)_lib_objs+=dbg_log_linux.c
-linux_$(_feat_)_lib_objs+=dbg_log_linux.c
 
 include $(PROJ_MAK_DIR)/epilog.mk
